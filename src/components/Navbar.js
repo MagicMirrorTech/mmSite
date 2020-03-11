7645ca3170aba650f0f5c2a3d0c0b0b5c212ce3c
diurivj <diurivj@icloud.com>
2020-03-11 16:48:30 -0600

    starting responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,8 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import theme from '../theme'
-import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
 import { Context } from '../context'
-import { Box } from '@chakra-ui/core'
+import { Box, MenuItems, Button, Text } from '@chakra-ui/core'
 
 const Nav = ({ children }) => (
   <Box
@@ -11,10 +10,11 @@ const Nav = ({ children }) => (
     w="100%"
     h="10vh"
     display="flex"
+    flexWrap="wrap"
     alignItems="center"
     justifyContent="space-between"
-    py="50px"
-    px="100px"
+    py={['25px', '50px']}
+    px={['25px', '100px']}
     boxSizing="border-box"
     backgroundColor="white"
   >
@@ -22,39 +22,54 @@ const Nav = ({ children }) => (
   </Box>
 )
 
-const Links = styled.div`
-  & * {
-    font-size: 20px;
-    margin-left: 100px;
-  }
-  & span:hover {
-    cursor: pointer;
-  }
-`
-
 const activeStyle = {
   color: theme.colors.mmorange
 }
 
-const imageContent = {
-  src: '/assets/MagicMirrorDevs_logo.png',
-  width: 80
-}
-
 function Navbar() {
   const { onOpen } = useContext(Context)
+  const [show, setShow] = useState(false)
 
   return (
-    <Nav>
-      <Box>
-        <NavLink exact to="/">
-          <img {...imageContent} alt="mm_logo" />
-        </NavLink>
+    <>
+      <Nav>
+        {/* logo */}
+        <Logo />
+
+        {/* mobile navbar */}
+        <Box onClick={() => setShow(!show)} display={['block', 'none']}>
+          <svg fill="black" width="24px" viewBox="0 0 20 20">
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </Box>
+
+        {/* desktop navbar */}
+        <DesktopNav onOpen={onOpen} />
+      </Nav>
+      <Box
+        display={[show ? 'block' : 'none', 'none']}
+        width={{ sm: 'full', md: 'auto' }}
+        alignItems="center"
+        flexGrow={1}
+      >
+        <Text>Docs</Text>
+        <Text>Examples</Text>
+        <Text>Blog</Text>
       </Box>
-      <Links>
+    </>
+  )
+}
+
+function DesktopNav({ onOpen }) {
+  return (
+    <Box fontSize="20px" display={['none', 'block']}>
+      <Box ml="100px" display="inline">
         <NavLink exact activeStyle={activeStyle} to="/">
           Home
         </NavLink>
+      </Box>
+      <Box ml="100px" display="inline">
         <a
           href="https://mmdevsblog.netlify.com"
           rel="noopener noreferrer"
@@ -62,11 +77,26 @@ function Navbar() {
         >
           Blog
         </a>
-        <Box as="span" onClick={onOpen}>
-          Contact us
-        </Box>
-      </Links>
-    </Nav>
+      </Box>
+      <Box ml="100px" as="span" cursor="pointer" onClick={onOpen}>
+        Contact us
+      </Box>
+    </Box>
+  )
+}
+
+function Logo() {
+  return (
+    <Box>
+      <NavLink exact to="/">
+        <Box
+          as="img"
+          width={[50, 80]}
+          src="/assets/MagicMirrorDevs_logo.png"
+          alt="mm_logo"
+        />
+      </NavLink>
+    </Box>
   )
 }
 
