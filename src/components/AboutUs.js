5d1ba9e766b94a82d445421bc8b0f669e4f2d74f
diurivj <diurivj@icloud.com>
2020-03-15 22:19:20 -0600

    about us medias done

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -5,38 +5,40 @@ function AboutUs({ onOpen }) {
   return (
     <Box
       w="100%"
-      minH="80vh"
+      minH={['auto', '50vh', '80vh', '80vh']}
       display="flex"
       boxSizing="border-box"
       px={[8, 8, 24, 24]}
       py={[4, 4, 16, 16]}
       bgImage="url('/assets/about_us.png')"
-      bgSize="cover"
+      bgSize={['150% 100%', '150% 100%', 'cover', 'cover']}
+      bgPos={['60%', '60%', '', '']}
       bgRepeat="no-repeat"
     >
-      <Stack w={['100%', '100%', '55%', '55%']} spacing={8}>
-        <Heading as="h1" fontSize={['2rem', '2rem', '2rem', '3rem']} letterSpacing="2px">
+      <Stack w={['100%', '100%', '100%', '55%']} spacing={8}>
+        <Heading as="h1" fontSize={['1.7rem', '1.7rem', '3rem', '3rem']} letterSpacing="2px">
           <span style={{ fontWeight: 'lighter' }}>May the odds </span>
           be ever in your favor
         </Heading>
-        <Text fontSize="xl">
+        <Text fontSize={['md', 'md', 'xl', 'xl']}>
           Magic Mirror is here to put the odds in your favor. Long gone are the days where competing
           against big companies felt like a losing battle.
         </Text>
-        <Text fontSize="xl">
+        <Text fontSize={['md', 'md', 'xl', 'xl']}>
           We are a group of highly efficient nerds, bridging the gap between fancy new technology
           and real revenue gains they promise.
         </Text>
         <Button
-          boxShadow="sm"
-          _hover={{ boxShadow: 'md' }}
-          _active={{ boxShadow: 'lg' }}
+          boxShadow="md"
+          _hover={{ boxShadow: 'lg' }}
+          _active={{ boxShadow: 'xl' }}
           backgroundColor="mmblue.500"
           variantColor="mmblue"
-          size="lg"
-          w="40%"
-          h="4rem"
+          size={['sm', 'sm', 'lg', 'lg']}
+          w={['60%', '60%', '40%', '40%']}
+          h={['2.6rem', '2.6rem', '4rem', '4rem']}
           onClick={onOpen}
+          alignSelf={['center', 'center', 'flex-start', 'flex-start']}
         >
           CREATE VALUE
         </Button>
